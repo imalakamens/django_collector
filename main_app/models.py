47b9988fafd7bcd8ca3a1f61246271ec1572c11e
Kamali Means <darenkmeans@gmail.com>
2020-09-15 19:49:42 -0700

    add model, remove dummy data... it works

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    album_title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    label = models.CharField(max_length=100)
+    year = models.IntegerField()
+    size = models.IntegerField()
+
+    def __str__(self):
+        return self.album_title
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
+from .models import Record
 
 # Create your views here.
 from django.http import HttpResponse
-
-class Record:
-    def __init__(self, album_title, artist, label, year, size):
-        self.album_title = album_title
-        self.artist = artist
-        self.label = label
-        self.year = year
-        self.size = size 
-
-records = [
-    Record('American Football', 'Americam Football', 'Polyvinyl Record Company', 2014, 12),
-    Record('Righteous Fists Of Harmony', 'Daedelus', 'Brainfeeder', 2010, 12),
-    Record('Cargo', 'Men At Work', 'Columbia', 1983, 12),
-    Record('Locust', 'The Locust', 'Gold Standard Laboratories', 1997, 7),
-]
 
 def home(request):
     return HttpResponse('<h1>(╯°□°）╯︵ ┻━┻</h1>')
@@ -25,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def records_index(request):
+    records = Record.objects.all()
     return render(request, 'records/index.html', { 'records' : records})
