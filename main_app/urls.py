6651fc62f27037d5df0ed992ebc8e1dff07e7bbd
Kamali Means <darenkmeans@gmail.com>
2020-09-16 20:22:04 -0700

    update edit view, add delete btn

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('records/', views.records_index, name='index'),
     path('records/<int:record_id>/', views.records_detail, name='detail'),
-    path('records/create',views.RecordCreate.as_view(), name='record_create'),
+    path('records/create/',views.RecordCreate.as_view(), name='records_create'),
+    path('records/<int:pk>/update/',views.RecordUpdate.as_view(), name='records_update'),
+    path('records/<int:pk>/delete/',views.RecordDelete.as_view(), name='records_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Record
 
 # Create your views here.
@@ -22,3 +22,11 @@ def records_detail(request, record_id):
 class RecordCreate(CreateView):
     model = Record
     fields = '__all__'
+
+class RecordUpdate(UpdateView):
+    model = Record
+    fields = ['artist','label','year','size']
+
+class RecordDelete(DeleteView):
+    model = Record
+    success_url = '/records/'
